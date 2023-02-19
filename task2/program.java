ff2906ffdc4eb45039cc098e293e30b70e815243
Elena Savinykh <gudulina@mail.ru>
2023-02-19 13:59:52 +0700

    Add new commit

--- task2/program.java
+++ task2/program.java
@@ -1,12 +1,19 @@
-//2. Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+//2. Реализуйте алгоритм сортировки пузырьком числового массива,
+// результат после каждой итерации запишите в лог-файл.
 
 package task2;
+
 import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class program {
     public static void main(String[] args) {
-        printArray(getArray());        
+        printArray(getArray());
         printArray(bubbleSort(getArray()));
+
     }
 
     // Создание массива
@@ -21,31 +28,32 @@ public class program {
     }
 
     // Сортировка массива
-    static int [] bubbleSort (int[] array) {
+    static int[] bubbleSort(int[] array) {
         int i = array.length - 1;
         for (i = array.length - 1; i >= 1; i--) {
             for (int j = 0; j < i; j++) {
-                if (array [j] > array [j+1])
-                    toTemp (array, j, j+1);
+                if (array[j] > array[j + 1])
+                    toTemp(array, j, j + 1);                    
             }
         }
         return array;
     }
 
-     // Поменять местами элементы массива
+    // Поменять местами элементы массива
     static void toTemp(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
 
+          
+        // Logger log = Logger.getLogger(program.class.getName());
+        // FileHandler fh = new FileHandler("log.txt");
+        // log.addHandler(fh);
 
-// Logger logger = Logger.getLogger(program.class.getName());
-// FileHandler fh = new FileHandler("log.txt");
-// logger.addHandler(fh);
-
-
-// SimpleFormatter sFormat = new SimpleFormatter();
-// fh.setFormatter(sFormat);
-
+        // SimpleFormatter sFormat = new SimpleFormatter();
+        // fh.setFormatter(sFormat);
+               
+        // Logger.log(Level.INFO, "Логирование 1");
+        // Logger.info("Логирование 2");
 }
